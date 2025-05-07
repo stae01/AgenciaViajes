253473d4a7c61ded4dae79f4bc9debfeb288351b
stae01 <166432863+stae01@users.noreply.github.com>
2025-05-06 18:53:03 -0700

    capa Persistencia

---
+++ persistenciaAgenciaViajes/src/main/java/daos/exceptions/NonexistentEntityException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daos.exceptions;
+
+/**
+ *
+ * @author 
+ */
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ persistenciaAgenciaViajes/src/main/java/daos/exceptions/PreexistingEntityException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daos.exceptions;
+
+/**
+ *
+ * @author 
+ */
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
+
